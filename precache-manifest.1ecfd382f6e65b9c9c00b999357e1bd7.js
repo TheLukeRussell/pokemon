5a14112733156463cbeed537349f908a6bd5488f
Luke Russell <thelukerussel21@gmail.com>
2020-05-07 15:01:44 -0400

    Updates

---
+++ precache-manifest.1ecfd382f6e65b9c9c00b999357e1bd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d74794e8bccd27ad0c8d3e3c6e4870",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "fec9c0c0a858cfe1c082",
+    "url": "/pokemon/static/css/main.ebc313c5.chunk.css"
+  },
+  {
+    "revision": "a041565f949ffbf8e421",
+    "url": "/pokemon/static/js/2.b5c1536f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.b5c1536f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec9c0c0a858cfe1c082",
+    "url": "/pokemon/static/js/main.f8799c06.chunk.js"
+  },
+  {
+    "revision": "ec6c7db34a75331294f4",
+    "url": "/pokemon/static/js/runtime-main.5ce2fed3.js"
+  }
+]);
